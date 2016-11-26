6984c366656d1eb03d35561c7466a88e33c15475
Daniel Miller <dmiller@cynosure.com>
2016-11-26 11:37:28 -0500

    made some changes to factory class

--- js/lib/DMShapeFactory.js
+++ js/lib/DMShapeFactory.js
@@ -8,23 +8,55 @@ define(['DMShape','DMRectangle', 'DMCircle'], function(DMShape, DMRectangle, DMC
    * @alias module: DMShapeFactory
    */
 
+   var _defaultShapeColor = '#fff';
   /**
-   * [function description]
-   * @param {String} shape The type of shape (either circle or square).
+   * Builds a shape based on arguments passed in.
    * @param {Object} args The arguments of the shape to be created.
    * @return {DMShape} returnShape The created shape to be returned.
    */
-  var buildShape = function(shape, args) {
+  var buildShape = function(args) {
+    args = _setDefaultShapeColor(args);
     var returnShape;
-    if(shape==='rectangle') {
-      returnShape = new DMRectangle(args);
-    } else if( shape==='circle') {
+    if(args.hasOwnProperty('radius')) {
       returnShape = new DMCircle(args);
+    } else if( args.hasOwnProperty('width') || args.hasOwnProperty('height')) {
+      args = _sanitizeRectParams(args);
+      returnShape = new DMRectangle(args);
     } else {
       returnShape = 'Invalid shape';
     }
 
     return returnShape;
+  };
+
+  /**
+   * Makes sure that both a width and height were specified. If not, it uses the value
+   * for either height or width depending on which one was specified.
+   * @param  {Object} args Shape args passed into the factory.
+   * @return {Object} args Returns the new shape arguments.
+   */
+  var _sanitizeRectParams = function(args) {
+    if(!args.hasOwnProperty('width')) {
+      args.width = args.height;
+    } else if(!args.hasOwnProperty('height')) {
+      args.height = args.width;
+    }
+
+    return args;
+  };
+
+  /**
+   * Makes sure that a color is set for the shape.
+   * @param  {Object} args Shape args passed into the factory
+   * @return {Object} args Returns a new set of args.  If no color was specified,
+   * it will use the _defaultShapeColor.
+   */
+  var _setDefaultShapeColor = function(args) {
+    if(!args.hasOwnProperty('color')) {
+      args.color = _defaultShapeColor;
+    }
+
+    return args;
   };
 
   return {
--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ requirejs.config({
 
 require(['DMCanvas', 'DMShapeFactory'], function(DMCanvas, DMShapeFactory){
 
-  args = {beginX: 35, beginY: 400, width: 100, height: 100, color: '#123456'};
-  var rect = DMShapeFactory.buildShape('rectangle', args);
+  args = {beginX: 35, beginY: 400, width: 100, color: '#123456'};
+  var rect = DMShapeFactory.buildShape(args);
   args = {beginX: 260, beginY: 200, width: 150, height: 150, color: '#135235'};
-  var rect2 = DMShapeFactory.buildShape('rectangle', args);
-  args = {beginX: 100, beginY: 120, radius: 100, color: '#246813'};
-  var circle = DMShapeFactory.buildShape('circle', args);
+  var rect2 = DMShapeFactory.buildShape(args);
+  args = {beginX: 100, beginY: 120, radius: 100};
+  var circle = DMShapeFactory.buildShape(args);
   DMCanvas.setShapes([rect, rect2, circle]);
   DMCanvas.setActiveShape(0);
   DMCanvas.buildCanvas();
